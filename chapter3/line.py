f0d8642c21a261f17eb65247aba3aca10da16132
Wolfram Donat <wolf@wolframdonat.com>
2017-06-11 11:43:52 -0700

    Adding Chapter 3 code

---
+++ chapter3/line.py
@@ -0,0 +1,25 @@
+from microbit import *
+ 
+a = Image('86420:86420:86420:86420:86420:')
+b = Image('68642:68642:68642:68642:68642:')
+c = Image('46864:46864:46864:46864:46864:')
+d = Image('24686:24686:24686:24686:24686:')
+e = Image('02468:02468:02468:02468:02468:')
+ 
+while True:
+    display.show(a)
+    sleep(200)
+    display.show(b)
+    sleep(200)
+    display.show(c)
+    sleep(200)
+    display.show(d)
+    sleep(200)
+    display.show(e)
+    sleep(200)
+    display.show(d)
+    sleep(200)
+    display.show(c)
+    sleep(200)
+    display.show(b)
+    sleep(200)
