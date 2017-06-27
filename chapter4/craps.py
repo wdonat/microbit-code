074ad7d8721f7bd269a057021c9bcd6055f14533
Wolfram Donat <wolf@wolframdonat.com>
2017-06-26 18:16:20 -0700

    Updating chapter 4 craps script

--- chapter4/craps.py
+++ chapter4/craps.py
@@ -1,6 +1,6 @@
 from microbit import *
+
 import random
- 
 random.seed()
 
 def roll_dice():
@@ -8,46 +8,39 @@ def roll_dice():
     b = random.randint(1, 6)
     numbers = [str(a), str(b)]
     return numbers
- 
+    
 def roll_seven():
     a = 4
     b = 3
     numbers = [str(a), str(b)]
     return numbers
- 
+    
 def roll_eight():
     a = 5
     b = 3
     numbers = [str(a), str(b)]
     return numbers
- 
+    
 def show_roll(dice):
-    for i in range(0, 2):
-        display.show(dice[0])
-        sleep(1000)
+    for i in range(0, len(dice)):
         display.clear()
-        display.show(dice[1])
+        display.show(dice[i])
         sleep(1000)
+    display.show(Image.YES)
+    
+display.show(Image.YES)
 
 while True:
- 
-    display.show(Image.YES)
-    if accelerometer.was_gesture("shake") and button_a.is_pressed():
-        display.clear()
-        sleep(500)
+    if not accelerometer.was_gesture("shake"):
+        continue
+    if button_a.is_pressed():
         roll = roll_seven()
         show_roll(roll)
- 
-    elif accelerometer.was_gesture("shake") and button_b.is_pressed():
-        display.clear()
-        sleep(500)
+        continue
+    if button_b.is_pressed():
         roll = roll_eight()
         show_roll(roll)
- 
-    elif accelerometer.was_gesture("shake"):
-        display.clear()
-        sleep(500)
-        roll = roll_dice()
-        show_roll(roll)
- 
-    sleep(1000)
+        continue
+    roll = roll_dice()
+    show_roll(roll)
+    
